a6c97ee8533045d84760bdcbfac3c7a995d0448e
alperenckr <alperenckroglu204@gmail.com>
2017-07-26 12:53:23 +0300

    Örnekler

---
+++ Raw Pointers and Smart Pointers/stdmove/cmake-build-debug/CMakeFiles/stdmove.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stdmove.dir/main.cpp.obj"
+  "stdmove.pdb"
+  "stdmove.exe"
+  "stdmove.exe.manifest"
+  "libstdmove.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stdmove.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
