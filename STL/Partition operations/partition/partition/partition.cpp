9f832eb88df4fbf74c039a580ab97fd2fc9fed43
ozdenurucar <ozdenuru@gmail.com>
2017-08-03 15:00:41 +0300

    partition_operation

---
+++ STL/Partition operations/partition/partition/partition.cpp
@@ -0,0 +1,42 @@
+﻿// partition.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+int main()
+{
+	// Vector tanımı
+	vector<int> vect = { 2, 1, 5, 6, 8, 7 };
+	is_partitioned(vect.begin(), vect.end(), [](int x)
+	{
+		return x % 2 == 0;
+
+	})?
+	cout << "Vector ayrilmis" :// is_partition fonksiyonundan true dönerse bu satır çalışır
+	cout << "Vector ayrilmamis"; //is_parttion fonksiyonundan false dönerse bu satır çalışır
+	cout << endl;
+	//2 ye tam bölünen sayıları ayırır
+	partition(vect.begin(), vect.end(), [](int x)//Ayırma,bölme, işlemi
+	{
+		return x % 2 == 0;
+
+	});
+	//Vectorun ayrılıp ayrılmadığını kontrol eder
+	is_partitioned(vect.begin(), vect.end(), [](int x)
+	{
+		return x % 2 == 0;
+
+	}) ?
+
+     cout << "Vektor ayrildi, partitioned isleminin sonucunda" :
+	cout << "Vektor hala ayrilamadi, partitioned isleminin sonucunda ";
+	cout << endl;
+	cout << "Ayrilmis hali: ";
+	for (int &x : vect) cout << x << " ";
+	cin.get();
+	return 0;
+}
---
+++ STL/Partition operations/partition_copy/partition_copy/partitioncopy.cpp
@@ -0,0 +1,25 @@
+﻿// partitioncopy.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <algorithm>
+#include <array>
+#include <iostream>
+#include <iterator>
+
+int main()
+{
+	std::array<int, 9> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+	auto is_even = [](int i) { return i % 2 == 0; };//is_partition benzeri bir kullanım
+	std::partition(v.begin(), v.end(), is_even);
+
+	auto p = std::partition_point(v.begin(), v.end(), is_even);
+
+	std::cout << "Before partition:\n    ";
+	std::copy(v.begin(), p, std::ostream_iterator<int>(std::cout, " "));
+	std::cout << "\nAfter partition:\n    ";
+	std::copy(p, v.end(), std::ostream_iterator<int>(std::cout, " "));
+	std::cin.get();
+	return 0;
+}
