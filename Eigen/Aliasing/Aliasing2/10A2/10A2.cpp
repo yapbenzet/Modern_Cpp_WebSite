593d34170443070fbab7c641c65e9fd529f665ce
boluknursena <150202049@kocaeli.edu.tr>
2017-12-06 03:25:26 -0800

    Add files via upload

---
+++ Eigen/Aliasing/Aliasing2/10A2/10A2.cpp
@@ -0,0 +1,19 @@
+//#include "stdafx.h" //Visual Studio icin.
+#include <iostream>
+#include <Eigen>
+using namespace std;
+using namespace Eigen;
+
+int main()
+{
+	Matrix2i a; a << 1, 2, 3, 4;
+	cout << "Here is the matrix a:\n" << a << endl;
+	a = a.transpose(); // !!! HATA !!!
+	cout << "and the result of the aliasing effect:\n" << a << endl;
+	/*
+		1 2		->		1 3			1 2
+		3 4				2 4			2 4
+					 (istenen)  (gerceklesen)
+	*/
+	cin.get();
+}
